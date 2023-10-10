b9c3ad640837260d8184d24e19182d6f58fe9464
Nagy Zétény <nagyzeteny6@gmail.com>
2023-10-10 13:52:14 +0200

    navbar constants

---
+++ src/constants/navItems.ts
@@ -0,0 +1,30 @@
+export const navItems = {
+  mainDishes: [
+    {
+      name: 'Milánói makaróni',
+      path: '/foetelek/milanoi-makaroni',
+    },
+    {
+      name: 'Roston csirkemell, rizs, tzatziki',
+      path: '/foetelek/roston-csirkemell',
+    },
+    {
+      name: 'Kijevi csirkemell',
+      path: '/foetelek/kijevi-csirkemell',
+    },
+  ],
+  deserts: [
+    {
+      name: 'Vörös bársony torta',
+      path: '/desszertek/voros-barsony-torta"',
+    },
+    {
+      name: 'Joghurt',
+      path: '/desszertek/joghurt',
+    },
+    {
+      name: 'Almás pite',
+      path: '/desszertek/almas-pite',
+    },
+  ],
+};
