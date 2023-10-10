e5b101600f344be5439b003d0dc21d7de23c64f7
Nagy Zétény <nagyzeteny6@gmail.com>
2023-10-10 16:57:26 +0200

    dynamic landing page, fix typo, image naming

--- src/constants/navItems.ts
+++ src/constants/navItems.ts
@@ -5,14 +5,23 @@ export const navItems = [
       {
         name: 'Milánói makaróni',
         path: '/foetelek/milanoi-makaroni',
+        image: '/images/milanoi-makaroni.jpg',
+        description:
+          'Izlese, fűszeres, pardicsomos darált hús áldentére főtőt makaróni ágyon, érlelt reszelt parmezán sajtal dízítet!',
       },
       {
         name: 'Roston csirkemell, rizs, tzatziki',
         path: '/foetelek/roston-csirkemell',
+        image: '/images/roston-csirkemell.jpg',
+        description:
+          'A ropogós roston sült csirkemell, illatos rizs és friss tzatziki harmonikus kombinációja. Az aranyló csirkehús és a puha rizs találkozása, melyet friss tzatziki saláta koronáz meg.',
       },
       {
         name: 'Kijevi csirkemell',
         path: '/foetelek/kijevi-csirkemell',
+        image: '/images/kijevi-csirkemell.png',
+        description:
+          'A Kijevi csirkemell egy híres és ízletes étel. Ez az étel egy különleges csirkemell töltelékéből áll, amelyet ropogós bundába burkolnak, majd sütnek.',
       },
     ],
   },
@@ -22,14 +31,23 @@ export const navItems = [
       {
         name: 'Vörös bársony torta',
         path: '/desszertek/voros-barsony-torta',
+        image: '/images/voros-barsony-torta.jpg',
+        description:
+          'A red velvet, vagyis a vörös bársony tökéletes elnevezése a süteménynek, mert nemcsak a színére utal, de tésztájának puha, selymes állagára is.',
       },
       {
         name: 'Joghurt',
         path: '/desszertek/joghurt',
+        image: '/images/joghurt.png',
+        description:
+          'A joghurt egy tejtermék, amelyet erjesztett tejből készítenek. Ez a finom és tápláló étel évszázadok óta része az emberi étrendnek, és számos kultúrában készül és fogyasztják.',
       },
       {
         name: 'Almás pite',
         path: '/desszertek/almas-pite',
+        image: '/images/almas-pite.jpg',
+        description:
+          'Arany barnára sült, gondosan elkészítet, omlós tésztában illatos, fahéjas, édes és egyben savanykás alma reszeléket ízlelhetünk!',
       },
     ],
   },
