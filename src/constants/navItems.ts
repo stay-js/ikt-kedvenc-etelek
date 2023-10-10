ba90b396fa7f027733d7a26673ee4e049b5cf174
Nagy Zétény <nagyzeteny6@gmail.com>
2023-10-10 15:17:38 +0200

    simplifying navbar

--- src/constants/navItems.ts
+++ src/constants/navItems.ts
@@ -1,30 +1,36 @@
-export const navItems = {
-  mainDishes: [
-    {
-      name: 'Milánói makaróni',
-      path: '/foetelek/milanoi-makaroni',
-    },
-    {
-      name: 'Roston csirkemell, rizs, tzatziki',
-      path: '/foetelek/roston-csirkemell',
-    },
-    {
-      name: 'Kijevi csirkemell',
-      path: '/foetelek/kijevi-csirkemell',
-    },
-  ],
-  deserts: [
-    {
-      name: 'Vörös bársony torta',
-      path: '/desszertek/voros-barsony-torta',
-    },
-    {
-      name: 'Joghurt',
-      path: '/desszertek/joghurt',
-    },
-    {
-      name: 'Almás pite',
-      path: '/desszertek/almas-pite',
-    },
-  ],
-};
+export const navItems = [
+  {
+    name: 'Főételek',
+    items: [
+      {
+        name: 'Milánói makaróni',
+        path: '/foetelek/milanoi-makaroni',
+      },
+      {
+        name: 'Roston csirkemell, rizs, tzatziki',
+        path: '/foetelek/roston-csirkemell',
+      },
+      {
+        name: 'Kijevi csirkemell',
+        path: '/foetelek/kijevi-csirkemell',
+      },
+    ],
+  },
+  {
+    name: 'Desszertek',
+    items: [
+      {
+        name: 'Vörös bársony torta',
+        path: '/desszertek/voros-barsony-torta',
+      },
+      {
+        name: 'Joghurt',
+        path: '/desszertek/joghurt',
+      },
+      {
+        name: 'Almás pite',
+        path: '/desszertek/almas-pite',
+      },
+    ],
+  },
+];
